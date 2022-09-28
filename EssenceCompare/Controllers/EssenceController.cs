e739d9397284f2658ebb977c7e73c0d8207ff6dc
King-Julian-Pool <78912385+King-Julian-Pool@users.noreply.github.com>
2022-09-28 10:41:11 +0200

    premier commit

---
+++ EssenceCompare/Controllers/EssenceController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace EssenceCompare.Controllers
+{
+    public class EssenceController : Controller
+    {
+        // 
+        // GET: /Essence/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /Essence/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ EssenceCompare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EssenceCompare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
